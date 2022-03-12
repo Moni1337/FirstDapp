e72b527df8d1251776447e0c932a6b943d6da283
Moni1337 <endosthetics@gmail.com>
2022-03-11 20:02:26 -0600

    import dist bootstrap folder,  create main.js, connect moralis info to script

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('Hello World');
+
+const serverUrl = "https://q47kygqhwxwc.usemoralis.com:2053/server";
+const appId = "TQhmDei5euu6lC8C9EeWx5jwOiIXiebqvogGVsTx";
+Moralis.start({ serverUrl, appId });
