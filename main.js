bbe80c96811601644dfa081aac2a0be169b7811c
Moni1337 <endosthetics@gmail.com>
2022-03-13 20:09:40 -0500

    added javascript error handling for dashboard page

--- main.js
+++ main.js
@@ -10,15 +10,35 @@ window.onload = function() {
 Moralis.initialize("4wJdPBQECgvFpJPRAXagDgaQRTY6QDDHPrwSAgLj");
 Moralis.serverURL = "https://ilzyoyhgikbf.usemoralis.com:2053/server";
 
+
+let homepage = "http://127.0.0.1:5500/dashboard.html";
+if( Moralis.User.current() == null && window.location.href == homepage){
+    document.getElementsByTagName("BODY")[0].style.display = "none";
+    window.location.href = "index.html";
+}
+
+
 login = async () => {
     await Moralis.Web3.authenticate().then(async function (user) {
-        console.log('Logged In');
-        user.set("name", document.getElementById('user-username').value);
-        user.set("email", document.getElementById('user-email').value);
-        await user.save();
-        window.location.href = "dashboard.html";
-        
+            user.set("name", document.getElementById('user-username').value);
+            user.set("email", document.getElementById('user-email').value);
+            await user.save();
+            console.log('Logged In');
+            window.location.href = "dashboard.html";
     })
 }
 
-document.getElementById('btn-login').onclick = login;
+logout = async () => {
+    await Moralis.User.logOut();
+    window.location.href = "index.html";
+}
+
+
+ if(document.getElementById('btn-login') != null){
+     document.getElementById('btn-login').onclick = login;
+    }
+
+ if(document.getElementById('btn-logout') != null){
+     document.getElementById('btn-logout').onclick = logout;
+ }
+
